47dcc84ad6bb88903889f318cdec4fdae0bb35d2
Limttugi <junhuk820@gmail.com>
2023-03-04 00:22:48 +0900

    style: 공통 CSS 분리

---
+++ src/styles/product.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StoreNameCS = styled.p`
+  font-weight: 400;
+  color: #767676;
+`;
+
+export const ProductNameCS = styled.p`
+  font-weight: 400;
+  color: black;
+`;
+
+export const PriceCS = styled.p`
+  display: flex;
+  align-items: baseline;
+  font-weight: 700;
+  color: black;
+`;
+
+export const WonCS = styled.sub`
+  color: black;
+`;
