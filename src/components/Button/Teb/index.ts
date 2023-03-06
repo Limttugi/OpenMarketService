20632bc507865f96d3839ae476e0920c5d499c71
Limttugi <junhuk820@gmail.com>
2023-03-06 11:24:56 +0900

    feat: Tep 메뉴 버튼 컴포넌트 추가

---
+++ src/components/Button/Teb/index.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { ButtonCS_Radius } from '..';
+
+export const TebMenuButtonCS = styled(ButtonCS_Radius)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: none;
+  height: 5rem;
+  font-size: 1.6rem;
+  font-weight: 500;
+  line-height: 2rem;
+  padding: 0 2rem;
+`;
+
+export const Alram = styled.div`
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  line-height: 2rem;
+  background-color: #eb5757;
+  border-radius: 50%;
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-align: center;
+  color: white;
+`;
